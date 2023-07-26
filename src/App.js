cc52154521a38e61a7b1ea865de06d707ab9096e
Rupesh malhotra <60328334+Rupesh0914@users.noreply.github.com>
2023-07-26 09:32:24 -0400

    sixth Commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
 import { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar';
 import TextForm from './components/TextForm/TextForm';
 import Alert from './components/Alert/Alert';
-// import About from './components/AboutUs/About';
+import About from './components/AboutUs/About';
 
 function App() {
-  const [mode, setMode] = useState('light'); // Dark Mode Enabled or no
-  const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState('light'); // Dark Mode Enabled or not
+  const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
-       setAlert({
-        msg: message,
-        type: type
-       })
-       setTimeout(() => {
-        setAlert(null)
-       }, 1500)
-  }
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
   const toggleMode = () => {
-    if(mode === 'light'){
-        setMode ('dark');
-        document.body.style.backgroundColor = '#042743';
-        showAlert("Dark mode anable", "success");
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode enabled", "success");
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = '#FFFFFF'; // Change this to the appropriate background color for light mode
+      showAlert("Light mode enabled", "success");
     }
-  else {
-    setMode ('light');
-    document.body.style.backgroundColor = '#042743'; 
-    showAlert("Light mode anable", "success")
-  }
-}
+  };
+
   return (
     <>
-    <Navbar title="Mitrex" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
-    <TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode}/>
-    {/* <About /> */}
+      <Router>
+        <Navbar title="Mitrex" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          {/* Define your routes using <Route> components */}
+          <Route exact path="/" element={<TextForm />} />
+          <Route exact path="/about" element={<About />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
- 
